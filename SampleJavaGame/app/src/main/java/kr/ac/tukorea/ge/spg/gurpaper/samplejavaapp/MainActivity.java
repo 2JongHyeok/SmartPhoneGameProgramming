8835f24db4639e73178d5402a1e0a22ce37e7107
Lee Jong Hyeok <gurpaper@naver.com>
2024-03-12 17:06:18 +0900

    Event 처리 방법 #3 Member Variable 활용

--- SampleJavaGame/app/src/main/java/kr/ac/tukorea/ge/spg/gurpaper/samplejavaapp/MainActivity.java
+++ SampleJavaGame/app/src/main/java/kr/ac/tukorea/ge/spg/gurpaper/samplejavaapp/MainActivity.java
@@ -1,7 +1,6 @@
 package kr.ac.tukorea.ge.spg.gurpaper.samplejavaapp;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -9,29 +8,31 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity{
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btn = findViewById(R.id.pushMeButton);
         //#2
-        btn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                TextView tv = findViewById(R.id.nameTextView);
-                tv.setText("PushMe");
-            }
-        });
-
+        Button btn = findViewById(R.id.pushMeButton);
+        btn.setOnClickListener(pushMeOnClickListener);
         Button btn2 = findViewById(R.id.anotherButton);
-        btn2.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                TextView tv = findViewById(R.id.nameTextView);
-                tv.setText("Another");
-            }
-        });
+        btn2.setOnClickListener(anotherOnClickListener);
     }
+
+    private View.OnClickListener pushMeOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("PushMe");
+        }
+    };
+
+    private View.OnClickListener anotherOnClickListener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("Another");
+        }
+    };
 }
