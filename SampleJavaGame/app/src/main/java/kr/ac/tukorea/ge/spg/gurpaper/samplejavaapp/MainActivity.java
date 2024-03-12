039bd6ca207efba9ddca6962b9869eff84b0d875
Lee Jong Hyeok <gurpaper@naver.com>
2024-03-12 16:53:45 +0900

    Initial commit

---
+++ SampleJavaGame/app/src/main/java/kr/ac/tukorea/ge/spg/gurpaper/samplejavaapp/MainActivity.java
@@ -0,0 +1,38 @@
+package kr.ac.tukorea.ge.spg.gurpaper.samplejavaapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.pushMeButton);
+        //#1.
+        btn.setOnClickListener(this);
+
+        Button btn2 = findViewById(R.id.anotherButton);
+        btn2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.pushMeButton){
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("PushMe");
+        }else if(id == R.id.anotherButton){
+            TextView tv = findViewById(R.id.nameTextView);
+            tv.setText("Another");
+        }
+        Log.d("Tag", "button clicked");
+    }
+}
