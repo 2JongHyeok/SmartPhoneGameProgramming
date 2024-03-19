d83a18e021e69974c588cfa1c99b5453fade7517
Lee Jong Hyeok <gurpaper@naver.com>
2024-03-19 16:30:45 +0900

    Image Res를 직접 지정한다.

--- Cards/app/src/main/java/kr/ac/tukorea/ge/spg/gurpaper/cards/MainActivity.java
+++ Cards/app/src/main/java/kr/ac/tukorea/ge/spg/gurpaper/cards/MainActivity.java
@@ -14,11 +14,28 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton previousImageButton;
+    private static int[] IMAGE_BTN_IDS = new int[]{
+      R.id.card_00,R.id.card_01,R.id.card_02,R.id.card_03,
+      R.id.card_10,R.id.card_11,R.id.card_12,R.id.card_13,
+      R.id.card_20,R.id.card_21,R.id.card_22,R.id.card_23,
+      R.id.card_30,R.id.card_31,R.id.card_32,R.id.card_33,
+    };
 
+    private static final int[] CARD_RES_IDS = new int[]{
+      R.mipmap.card_as,R.mipmap.card_as,R.mipmap.card_2c,R.mipmap.card_2c,
+      R.mipmap.card_3d,R.mipmap.card_3d,R.mipmap.card_4h,R.mipmap.card_4h,
+      R.mipmap.card_5s,R.mipmap.card_5s,R.mipmap.card_jc,R.mipmap.card_jc,
+      R.mipmap.card_qh,R.mipmap.card_qh,R.mipmap.card_kd,R.mipmap.card_kd,
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        for(int i = 0; i < IMAGE_BTN_IDS.length; ++i){
+            ImageButton ib = findViewById(IMAGE_BTN_IDS[i]);
+            ib.setImageResource(CARD_RES_IDS[i]);
+        }
     }
 
     public void onBtnCard(View view) {
@@ -28,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
         if(previousImageButton == imageButton){
             return;
         }
+
         if (previousImageButton != null) {
             previousImageButton.setImageResource(R.mipmap.card_blue_back);
         }
